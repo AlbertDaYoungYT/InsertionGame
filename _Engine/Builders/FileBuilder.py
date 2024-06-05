eaac56001000ef6cfedcdaac59a85222c1330126
Albert Knudsen <65977153+AlbertDaYoungYT@users.noreply.github.com>
2024-06-05 12:05:40 +0200

    File Builder

--- _Engine/Builders/FileBuilder.py
+++ _Engine/Builders/FileBuilder.py
@@ -1,4 +1,19 @@
+from pathlib import Path
 import sys
 import os
 
+HOME = os.environ["INSERTION_HOME"]
+USER = str(Path.home())
+
+
 def run():
+    if not os.path.exists(os.path.join(HOME, "Data/")): os.mkdir(os.path.join(HOME, "Data/"))
+    if not os.path.exists(os.path.join(HOME, "Content/")): os.mkdir(os.path.join(HOME, "Content/"))
+    if not os.path.exists(os.path.join(HOME, "DLCs/")): os.mkdir(os.path.join(HOME, "DLCs/"))
+    if not os.path.exists(os.path.join(HOME, "Languages/")): os.mkdir(os.path.join(HOME, "Languages/"))
+    if not os.path.exists(os.path.join(HOME, "Screens/")): os.mkdir(os.path.join(HOME, "Screens/"))
+
+
+#    if not os.path.exists(os.path.join(USER, "AppData/Local/Insertion/")): os.mkdir(os.path.join(HOME, "AppData/Local/Insertion/"))
+#    if not os.path.exists(os.path.join(USER, "AppData/Local/Insertion/Saves")): os.mkdir(os.path.join(HOME, "AppData/Local/Insertion/Saves"))
+    
--- _Engine/__init__.py
+++ _Engine/__init__.py
@@ -18,23 +18,39 @@ import os
 
 class Engine:
 
-    def __init__(self, id, config):
+    def __init__(self, id, config, *args, **kwargs):
         self.id = id
         self.config = config
 
         self.locale = locale.getdefaultlocale()[0]
 
-        self.log = LogManager.Log(self)
-        self.system = SystemManager.System(self)
-        self.language = LanguageManager.Language(self)
-        self.cache = CacheManager.Cache(self)
-        self.time = TimeManager.Time(self)
-        self.state = StateManager.State(self)
-        self.content = ContentManager.Content(self)
-        self.save = SaveManager.Save(self)
-        self.server = ServerManager.Server(self)
+        self.log = LogManager.Log(self, *args, **kwargs)
+        self.system = SystemManager.System(self, *args, **kwargs)
+        self.language = LanguageManager.Language(self, *args, **kwargs)
+        self.cache = CacheManager.Cache(self, *args, **kwargs)
+        self.time = TimeManager.Time(self, *args, **kwargs)
+        self.state = StateManager.State(self, *args, **kwargs)
+        self.content = ContentManager.Content(self, *args, **kwargs)
+        self.save = SaveManager.Save(self, *args, **kwargs)
+        self.server = ServerManager.Server(self, *args, **kwargs)
 
+        self.init("./Builders/")
         self.loadPlugins()
+    
+    def init(self, dir):
+        imported_modules = []
+        current_directory = os.path.join(os.path.dirname(__file__), "./Plugins/")
+
+        for filename in os.listdir(current_directory):
+            if filename.endswith('.py') and filename != '__init__.py':
+                module_name = filename[:-3]
+                importlib.import_module(f".{module_name}", package=__name__)
+                imported_modules.append(module_name)
+
+        for module in imported_modules:
+            if hasattr(module, 'run') and callable(getattr(module, 'run')):
+                module.run(self)
+
     
     def loadPlugins(self):
         imported_modules = []
